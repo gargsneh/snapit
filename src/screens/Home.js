cd3eb806a129df7d7d1e0dad1d77b5eae4d84479
gargsneh <gargsneh58@gmail.com>
2023-12-29 00:27:11 +0530

    second

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,10 +2,20 @@ import React from 'react'
 import Navbar from '../component/Navbar'
 import Footer from '../component/Footer'
 
+
 export default function Home() {
   return (<>
     <div> <Navbar/> </div>
-    <div> boody</div>
+    <div> 
+    <div className ="card mt-3" style ={{"width": "18rem" , "maxHeight" : "300px"} }>
+  <img src="..." className ="card-img-top" alt="..."/>
+  <div className ="card-body">
+    <h5 className ="card-title">Card title</h5>
+    <p className ="card-text">Some quick example </p>
+  
+  </div>
+</div>
+    </div>
     <div> <Footer/> </div>
     </>
   )
