31771a5e092909983357e200898d3e0c80aae624
gargsneh <gargsneh58@gmail.com>
2023-12-28 20:27:12 +0530

    first

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Footer from '../component/Footer'
+
+export default function Home() {
+  return (<>
+    <div> <Navbar/> </div>
+    <div> boody</div>
+    <div> <Footer/> </div>
+    </>
+  )
+}
