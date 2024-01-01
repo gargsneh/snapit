38f8f178f598f7f3093ff5af667b76bed176017e
gargsneh <gargsneh58@gmail.com>
2024-01-01 19:05:03 +0530

    backend

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+/*const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://gargsneh40:F4MNiaR5CchZLHVe@database1.zasmyy7.mongodb.net/?retryWrites=true&w=majority'
+
+const mongodb=() => {
+    mongoose.connect(mongoURI , ()=>(
+        console.log("connected")
+    ));
+
+    }
+
+    module.exports = mongodb;*/
