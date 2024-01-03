60e28773ffbf38e256a73fd13f7e1333998bc416
gargsneh <gargsneh58@gmail.com>
2024-01-03 18:53:56 +0530

    process

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 async function connectToDatabase() {
     try {
+
       await mongoose.connect('mongodb+srv://gargsneh40:F4MNiaR5CchZLHVe@database1.zasmyy7.mongodb.net/fooditems?retryWrites=true&w=majority');
+
       console.log('Connected to database successfully');
-      const collection =  mongoose.connection.db.collection("delivery");
-      const data = await collection.find({}).toArray(function(err,data){
-        console.log(data);
-      });
+
+      const collection =   await mongoose.connection.db.collection("delivery");
+      const data = await collection.find({}).toArray();
+      console.log('Fetched data:', data);
+      }
         
             
         
         
-      }
+      
      catch (err) {
       console.error('Error connecting to database:', err);
     }
